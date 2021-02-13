e00182d337aff327950ac8887abac9a183e357a0
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-02-12 22:45:09 -0300

    calculando as horas, mas os dias e minutos não aparecem na tela

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,37 @@
-const dataAlvo = "1 Jan 2022";
+window.onload = function () {
+  countdown();
+  setInterval(countdown, 1000);
+};
+
+const daysEl = document.getElementById("dias_c");
+const hoursEl = document.getElementById("horas_c");
+const minutesEl = document.getElementById("minutos");
+const secondsEl = document.getElementById("segundos");
+const dataAlvo = "1 Jan 2022 00:00:00";
 
 function countdown() {
   const data = new Date(dataAlvo);
   const hoje = new Date();
 
   const segundosTotal = (data - hoje) / 1000;
+
   const dias = Math.floor(segundosTotal / 3600 / 24);
   const horas = Math.floor((segundosTotal / 3600) % 24);
-  const minutos = Math.floor(segundosTotal / 60) % 60;
+  const minutes = Math.floor(segundosTotal / 60) % 60;
   const segundos = Math.floor(segundosTotal) % 60;
 
-  console.log(dias, horas, minutos, segundos);
+  daysEl.innerHTML = formatDate(dias);
+  hoursEl.innerHTMLL = horas_c;
+  minutesEl.innerHTMLL = formatDate(minutes);
+  secondsEl.innerHTML = formatDate(segundos);
+
+  //   console.log(daysEl);
+  console.log(hoursEl);
+  //   console.log(minutesEl);
+  //   console.log(secondsEl);
+  console.log(dias, horas, minutes, segundos);
 }
 
-countdown();
-
-setInterval(countdown, 1000);
+function formatDate(time) {
+  return time < 10 ? `0${time}` : time;
+}
