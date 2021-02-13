e02400939645133bdc705fea5f7251c9a88bef7e
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-02-12 22:54:35 -0300

    Codigo JS ajustado, agora está mostrando os dias e horas corretos, o erro estava na digitação do innerHTML e também buscando variaveis que não existiam

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,5 @@
-window.onload = function () {
-  countdown();
-  setInterval(countdown, 1000);
-};
-
-const daysEl = document.getElementById("dias_c");
-const hoursEl = document.getElementById("horas_c");
+const daysEl = document.getElementById("dias");
+const hoursEl = document.getElementById("horas");
 const minutesEl = document.getElementById("minutos");
 const secondsEl = document.getElementById("segundos");
 const dataAlvo = "1 Jan 2022 00:00:00";
@@ -21,12 +16,12 @@ function countdown() {
   const segundos = Math.floor(segundosTotal) % 60;
 
   daysEl.innerHTML = formatDate(dias);
-  hoursEl.innerHTMLL = horas_c;
-  minutesEl.innerHTMLL = formatDate(minutes);
+  hoursEl.innerHTML = formatDate(horas);
+  minutesEl.innerHTML = formatDate(minutes);
   secondsEl.innerHTML = formatDate(segundos);
 
   //   console.log(daysEl);
-  console.log(hoursEl);
+  //   console.log(hoursEl);
   //   console.log(minutesEl);
   //   console.log(secondsEl);
   console.log(dias, horas, minutes, segundos);
@@ -35,3 +30,6 @@ function countdown() {
 function formatDate(time) {
   return time < 10 ? `0${time}` : time;
 }
+
+countdown();
+setInterval(countdown, 1000);
